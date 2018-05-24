98d52c0052e69b619974a8d3d40b7946b0417049
Tomas Winkler <tomas.winkler@intel.com>
2018-05-24 14:57:02 +0300

    rpmb: VRPMB-FE create virtio rpmb frontend driver
    This patch implements virtio rpmb frontend driver.
    The driver will work with RPMB VBS-U together to
    provide one communication channel between UOS and SOS.
    
    V2: 1. Change license to dual BSD/GPL
        2. Fix coding style.
        3. Use pr_fmt macro instead of ERR, DBG, ...
    V3: 1. Replace - with _ in file name.
        2. Plug to rpmb framework instead of using own misc device
        3. Use arrays of scatter lists instead of linearizing the data.
    V4: 1. Allocate memory for control structures, it's not possible to DMA
           from the stack.
    V5: 1. Add mutex and use wait queue instead of completion.
        2. WIP code for getting capabilities
    V6: 1. Fix calculation of the allocation size for seq cmd
        2. WIP code for getting capabilities
        3. Drop unused constant RPMB_MAX_FRAMES
    V7: 1. Set auth algorithm
    
    Change-Id: I88a42f2e8f2ea1573aad9b5cafeae812c669a73e
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    
    ---
    ajb:
      - add include linux/slab.h for kzalloc
      - include standardised VIRTIO_ID_RPMB
      - remove extra frames when sending commands down virtqueue
      - clean up out/in sgs - hopefully more conforming
      - remove cmd_cap code

--- include/uapi/linux/virtio_ids.h
+++ include/uapi/linux/virtio_ids.h
@@ -47,6 +47,7 @@
 #define VIRTIO_ID_MEM          24 /* virtio mem */
 #define VIRTIO_ID_FS           26 /* virtio filesystem */
 #define VIRTIO_ID_PMEM         27 /* virtio pmem */
+#define VIRTIO_ID_RPMB         28 /* virtio RPMB */
 #define VIRTIO_ID_MAC80211_HWSIM 29 /* virtio mac80211-hwsim */
 #define VIRTIO_ID_VIDEO_ENC    30 /* virtio video encoder */
 #define VIRTIO_ID_VIDEO_DEC    31 /* virtio video decoder */
